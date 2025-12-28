937814abcf0d1bb89a3383968a2c990e3db3e42d
Soren2177 <134076474+Soren2177@users.noreply.github.com>
2025-12-28 17:53:37 +0530

    Add default environment variable values
    Set default values for API_ID, API_HASH, BOT_TOKEN, and BOT_USERNAME.

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "25000803"))
+    API_HASH = os.environ.get("API_HASH", "e2132a1280d0e6cd29a6e6fef064e112")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8546230285:AAEQTaODc5gmtgsG1tiPDmZaGzY6G0XOa18")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "sorensnapchatdownloaderbot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
